20885a91823b4651b9f35ed82c6209a767e4d1fe
Ole <ole@cf86aa31-c784-44f0-a929-dfc687923ddc.fritz.box>
2025-06-14 19:05:48 +0200

    Enable flyto and native leaflet

--- client/src/app/Map.tsx
+++ client/src/app/Map.tsx
@@ -1,11 +1,7 @@
-import dynamic from "next/dynamic";
 import React, { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
 import { Event } from "./EventExplorer";
-
-const MapContainer = dynamic(() => import("react-leaflet").then(mod => mod.MapContainer), { ssr: false });
-const TileLayer = dynamic(() => import("react-leaflet").then(mod => mod.TileLayer), { ssr: false });
-const Circle = dynamic(() => import("react-leaflet").then(mod => mod.Circle), { ssr: false });
+import L, { LatLngTuple } from 'leaflet';
 
 interface MapComponentProps {
 	event?: Event
@@ -19,57 +15,60 @@ interface Entry {
 const colors = ["blue", "red", "orange", "yellow", "green", "purple"]
 const MapComponent = ({ event }: MapComponentProps) => {
 	const [locationLookup, setLocationLookup] = useState<Record<string, Entry[]>>({});
-	const [pointsToMark, setPointsToMark] = useState<Array<Array<{ position: number[], key: string }>>>([])
+	const [map, setMap] = useState<L.Map | undefined>();
+	const [markerLayer, setMarkerLayer] = useState<L.LayerGroup | undefined>();
 
 	useEffect(() => {
-		// Beispiel: JSON lokal importieren
 		import("./locations.json").then((data) => {
 			setLocationLookup(data.default || data);
 		});
 	}, []);
-	// Example bounds for Berlin (adjust as needed)
-	const berlinBounds = [
-		[52.3383, 13.0884], // Southwest corner
-		[52.6755, 13.7611], // Northeast corner
-	];
 
-	// Center of Berlin
-	const berlinCenter = [52.5200, 13.4050];
+	const berlinCenter: LatLngTuple = [52.5200, 13.4050];
 
 	useEffect(() => {
-		if (!event) {
+		if (!event || !markerLayer) {
 			return
 		}
+		markerLayer?.clearLayers()
 		const { way_points: ways } = event;
-		const locations = ways.map((way, way_index) => way.map((point, index) => {
+		let firstLocation: LatLngTuple | undefined = undefined
+		ways.map((way, way_index) => way.map((point) => {
 			const location = locationLookup[point][0]
 			if (!location) {
 				return
 			}
-			return { key: `${way_index}-${index}`, position: [parseFloat(location.lat), parseFloat(location.lon)] }
-		}).filter(entry => entry !== undefined))
-		setPointsToMark(locations)
-	}, [event, locationLookup])
+			const position: LatLngTuple = [parseFloat(location.lat), parseFloat(location.lon)]
+			if (!firstLocation) {
+				firstLocation = position
+			}
+			L.circle(position, {
+				color: colors[way_index],
+				fillColor: colors[way_index],
+				radius: 200
+			}).addTo(markerLayer);
+		}))
+		if (!!firstLocation) {
+			map?.flyTo(firstLocation, 12)
+		}
+	}, [event, locationLookup, map, markerLayer])
 
+	useEffect(() => {
+		const map = L.map('map').setView(berlinCenter, 13);
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; OpenStreetMap contributors'
+		}).addTo(map);
+		setMarkerLayer(L.layerGroup().addTo(map));
+		setMap(map)
+		return () => {
+			map.remove()
+		}
+	}, []);
 	return (
 		<div className="w-full h-40">
-			<MapContainer
-				center={berlinCenter}
-				zoom={12}
-				minZoom={10}
-				maxZoom={16}
-				maxBounds={berlinBounds}
-				maxBoundsViscosity={1.0} // Prevents panning outside bounds
+			<div id="map"
 				style={{ height: "100%", width: "100%" }}
-			>
-				<TileLayer
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-				/>
-				{pointsToMark.map((points, index) => points.map(({ position, key }) => (
-					<Circle key={key} center={position} radius={200} pathOptions={{ color: colors[index] }} />
-				)))}
-			</MapContainer>
+			/>
 		</div>
 	);
 };
