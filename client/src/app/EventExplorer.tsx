f21d0602ab5e13b840706fd8c0f05ceae9383e50
Ole <ole@Mac.fritz.box>
2025-06-17 20:05:22 +0200

    Adjust and refactor filter options

--- client/src/app/EventExplorer.tsx
+++ client/src/app/EventExplorer.tsx
@@ -1,18 +1,23 @@
 "use client"
 import { useState, useEffect } from "react";
-import { addDays, isWithinInterval, parse } from "date-fns";
+import { addDays, isAfter, parse, isBefore } from "date-fns";
 import MapComponent from "./Map";
 import type { LatLngBounds } from "leaflet";
 import { Event as EventComponent } from "./Event";
 import { Event, useEvents } from "./useEvents";
+import { FilterButton } from "./FilterButton";
+import { isEqual } from "date-fns/fp";
+
+type TimeFilter = "past" | "future" | "today" | "tomorrow"
 
 export default function EventExplorer() {
 	const events = useEvents();
 	const [filteredEvents, setFilteredEvents] = useState<Event[]>([]);
 	const [selectedEvent, setSelectedEvent] = useState<Event | undefined>();
 	const [keyword, setKeyword] = useState("");
-	const [timeFilter, setTimeFilter] = useState("all");
-	const [boundsFilter, setBoundsFilter] = useState<LatLngBounds | undefined>(undefined);
+	const [timeFilter, setTimeFilter] = useState<TimeFilter>("future");
+	const [filterByMapBounds, setFilterByMapBounds] = useState(false);
+	const [mapBounds, setMapBounds] = useState<LatLngBounds | undefined>(undefined);
 
 	useEffect(() => {
 		setFilteredEvents(events)
@@ -20,7 +25,7 @@ export default function EventExplorer() {
 
 	useEffect(() => {
 		applyFilters();
-	}, [keyword, timeFilter, events, boundsFilter]);
+	}, [keyword, timeFilter, events, mapBounds, filterByMapBounds]);
 
 	const applyFilters = () => {
 		let result = [...events];
@@ -32,25 +37,54 @@ export default function EventExplorer() {
 				)
 			);
 		}
-
-		if (timeFilter == "week") {
+		if (timeFilter === "past") {
 			const now = new Date();
-			const endDate =
-				timeFilter === "week" ? addDays(now, 7) : addDays(now, 30);
-
 			result = result.filter((e) => {
 				const date = parse(e.date[0], "dd.MM.yyyy", new Date());
-				return isWithinInterval(date, { start: now, end: endDate });
+				return isBefore(date, now);
 			});
 		}
-
-		if (boundsFilter !== undefined && timeFilter === "map") {
-			result = result.filter(e => e.way_points.some(point => boundsFilter?.contains(point.position)))
+		if (timeFilter === "future") {
+			const now = new Date();
+			now.setHours(0, 0, 0, 0)
+			result = result.filter((e) => {
+				const date = parse(e.date[0], "dd.MM.yyyy", new Date());
+				console.log(date)
+				return isAfter(date, now) || isEqual(date, now);
+			});
+		}
+		if (timeFilter === "today") {
+			const now = new Date();
+			now.setHours(0, 0, 0, 0)
+			result = result.filter((e) => {
+				const date = parse(e.date[0], "dd.MM.yyyy", new Date());
+				console.log(date)
+				return isEqual(date, now);
+			});
+		}
+		if (timeFilter === "tomorrow") {
+			const now = new Date();
+			now.setHours(0, 0, 0, 0)
+			const tomorrow = addDays(now, 1)
+			result = result.filter((e) => {
+				const date = parse(e.date[0], "dd.MM.yyyy", new Date());
+				return isEqual(date, tomorrow);
+			});
+		}
+		if (mapBounds !== undefined && filterByMapBounds) {
+			result = result.filter(e => e.way_points.some(point => mapBounds?.contains(point.position)))
 		}
 		setFilteredEvents(result);
 	};
 	const onMapChange = (bounds: LatLngBounds) => {
-		setBoundsFilter(bounds);
+		setMapBounds(bounds);
+	}
+	const toggleFilter = (inputFilter: TimeFilter) => () => {
+		if (inputFilter === timeFilter) {
+			setTimeFilter("future")
+			return
+		}
+		setTimeFilter(inputFilter)
 	}
 
 
@@ -60,7 +94,7 @@ export default function EventExplorer() {
 				<h1 className="text-xl font-bold">Event Explorer</h1>
 				<h2 className="text-sm">Stand 16. Juni 25 21:00</h2>
 			</div>
-			<div className="flex flex-col sm:flex-row gap-2">
+			<div className="flex flex-col gap-2">
 				<input
 					type="text"
 					placeholder="Suche nach Keyword..."
@@ -68,31 +102,24 @@ export default function EventExplorer() {
 					value={keyword}
 					onChange={(e) => setKeyword(e.target.value)}
 				/>
-				<div className="flex gap-2">
-					<button
-						onClick={() => setTimeFilter("all")}
-						className={`py-1 px-2 border rounded ${timeFilter === "all" ? "bg-blue-500 text-white" : "bg-white"
-							}`}
-					>
-						Alle
-					</button>
-					<button
-						onClick={() => setTimeFilter("week")}
-						className={`py-1 px-2 border rounded ${timeFilter === "week" ? "bg-blue-500 text-white" : "bg-white"
-							}`}
-					>
-						Nächste Woche
-					</button>
-					<button
-						onClick={() => setTimeFilter("map")}
-						className={`py-1 px-2 border rounded ${timeFilter === "map" ? "bg-blue-500 text-white" : "bg-white"
-							}`}
-					>
+				<div className="flex justify-between">
+					<div className="flex gap-2">
+						{(timeFilter === "today" || timeFilter === "future") && <FilterButton onClick={toggleFilter("today")} enabled={timeFilter === "today"}>
+							Heute
+						</FilterButton>}
+						{(timeFilter === "tomorrow" || timeFilter === "future") && <FilterButton onClick={toggleFilter("tomorrow")} enabled={timeFilter === "tomorrow"}>
+							Morgen
+						</FilterButton>}
+						{(timeFilter === "past" || timeFilter === "future") && <FilterButton onClick={toggleFilter("past")} enabled={timeFilter === "past"}>
+							Vergangene
+						</FilterButton>}
+					</div>
+					<FilterButton onClick={() => setFilterByMapBounds(state => !state)} enabled={filterByMapBounds}>
 						Auf Karte
-					</button>
+					</FilterButton>
 				</div>
 			</div>
-			<MapComponent event={selectedEvent} onMapChange={onMapChange} disableFlyTo={timeFilter === "map"} />
+			<MapComponent event={selectedEvent} onMapChange={onMapChange} disableFlyTo={filterByMapBounds} />
 			{filteredEvents.length} Events
 			{filteredEvents.length === 0 ? (
 				<p className="text-gray-500">Keine Events gefunden.</p>
---
+++ client/src/app/FilterButton.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+
+interface FilterButtonProps {
+	onClick: () => void;
+	children: ReactNode;
+	enabled: boolean;
+}
+export const FilterButton = ({ onClick, children, enabled }: FilterButtonProps) => {
+	return (<button
+		onClick={onClick}
+		className={`px-2 py-1 text-sm rounded-full flex items-center gapx-2 ${enabled ? "bg-blue-500 text-white" : "bg-white border"
+			}`}
+	>
+		<div>{children}</div>
+		{enabled && <span className="material-icons text-white" style={{ fontSize: '16px' }}>
+			close
+		</span>}
+	</button>
+	)
+}
